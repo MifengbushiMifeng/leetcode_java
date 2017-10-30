7b7d8814b0ab67c12834d683fb878cb027e60baa
Jonathan Zhou <zhouronghui30@163.com>
2017-10-30 13:48:29 +0800

    leetcode of twosum in O(n)

--- src/main/java/algorithms/twosum/TwoSum.java
+++ src/main/java/algorithms/twosum/TwoSum.java
@@ -1,6 +1,5 @@
 package algorithms.twosum;
 
-
 /**
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * <p>
---
+++ src/main/java/algorithms/twosum/TwoSum_O_n.java
@@ -0,0 +1,34 @@
+package algorithms.twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum_O_n {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        /*int[] result = new int[2];*/
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                /*result[1] = i + 1;
+                result[0] = map.get(target - numbers[i]);
+                return result;*/
+                return new int[]{map.get(target - numbers[i]), i};
+            }
+            map.put(numbers[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+        /*return result;*/
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + " / " + result[1]);
+
+    }
+}
