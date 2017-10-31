e1f3750c9a1a44908c4ec0458e15eafd1894eea2
Jonathan Zhou <zhouronghui30@163.com>
2017-10-31 17:29:53 +0800

    leetcode of add two numbers in O(n)

---
+++ src/main/java/algorithms/addtwonumbers/AddTwoNumbers_O_n.java
@@ -0,0 +1,36 @@
+package algorithms.addtwonumbers;
+
+/**
+ * Created by CISCO : jonzhou on 2017/10/31.
+ * <p>
+ * The usage of this class is ...
+ */
+public class AddTwoNumbers_O_n {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummyHead = new ListNode(0);
+
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        return dummyHead.next;
+    }
+}
