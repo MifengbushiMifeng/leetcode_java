465c29cf830c3689534c2e2792c2a57c42b13e91
Jonathan Zhou <zhouronghui30@163.com>
2017-11-16 14:50:14 +0800

    escape analysis / Do NOT get the point yet!

---
+++ src/main/java/other/escape/analysis/EscapAnalysisTest.java
@@ -0,0 +1,19 @@
+package other.escape.analysis;
+
+public class EscapAnalysisTest {
+
+    public static void alloc() {
+        byte[] b = new byte[2];
+        b[0] = 1;
+    }
+
+    public static void main(String[] args) {
+
+        long b = System.currentTimeMillis();
+        for (int i = 0; i < 100000000; i++) {
+            alloc();
+        }
+        long e = System.currentTimeMillis();
+        System.out.print(e - b);
+    }
+}
