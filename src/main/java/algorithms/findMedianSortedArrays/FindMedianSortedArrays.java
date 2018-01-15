e552dbbc1b96f2370fc96689060ab4d102e868a0
Jonathan Zhou <zhouronghui30@163.com>
2018-01-15 14:00:05 +0800

    median of two sorted arrays

--- src/main/java/algorithms/findMedianSortedArrays/FindMedianSortedArrays.java
+++ src/main/java/algorithms/findMedianSortedArrays/FindMedianSortedArrays.java
@@ -22,19 +22,72 @@ package algorithms.findMedianSortedArrays;
  */
 public class FindMedianSortedArrays {
 
-    public static double findMedianSortedArrays(int[] num1, int[] num2) {
+    private static double findMedianSortedArrays(int[] num1, int[] num2) {
 
-        double result = 0;
+        int m = num1.length;
+        int n = num2.length;
+
+        // ensure  m <= n
+        if (m > n) {
+            int[] tmp = num1;
+            num1 = num2;
+            num2 = tmp;
+            m = num1.length;
+            n = num2.length;
+        }
+
+        int iMin = 0;
+        int iMax = m;
+        int halfLen = (m + n + 1) / 2;
+
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+
+            // i is too small
+            if (i < iMax && num2[j - 1] > num1[i]) {
+                iMin = iMin + 1;
+            }
+
+            // i is too big
+            else if (i > iMin && num2[j] < num1[i - 1]) {
+                iMax = iMax - 1;
+            }
+            // i is perfect
+            else {
+                int maxLeft = 0;
+
+                if (i == 0) {
+                    maxLeft = num2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = num1[i - 1];
+                } else {
+                    maxLeft = Math.max(num1[i - 1], num2[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = num2[j];
+                } else if (j == n) {
+                    minRight = num1[i];
+                } else {
+                    minRight = Math.min(num2[j], num1[i]);
+                }
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
 
 
-
-        return result;
+        return 0.0;
     }
 
     public static void main(String[] args) {
 
-        int[] num1 = {1, 3, 5, 7};
-        int[] num2 = {2, 4, 11, 15};
+        int[] num1 = {};
+        int[] num2 = {1};
         System.out.println(findMedianSortedArrays(num1, num2));
     }
 
